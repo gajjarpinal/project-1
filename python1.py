fa397bf8c1e30961f4fbd1f33703645b188651e4
gajjarpinal <gajjarpinal1@gmail.com>
2026-05-19 17:45:35 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,23 @@
+print("hello sir, welcome ! please enter your details in below \n")
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+city=input("Enter your city:")
+favourite_food=input("Enter your favourite food:")
+
+current_year=2026
+birth_year=current_year-age
+
+print("Name:",name)
+print("Age:",age)
+print("City:",city)
+print("Favourite_Food:",favourite_food)
+print("Your birth year is approximately:",birth_year)
+
+print("name=",name,"|Type:",type(name))
+print("Age=",age,"|Type:",type(age))
+print("city=",city,"|Type:",type(city))
+print("favourite_food=",favourite_food,"|Type:",type(favourite_food))
+
+print("Thank you sir ! for your detail.")
+     
